740bbd9edb6e3541b35f9c8bddb176567f5886b9
Dmitriy Grudistov <dgrudistov@primeaspect.io>
2021-07-31 09:24:09 +0300

    Создан родтельский класс TestBase В TestBase перенесены базовые данные для тестов Класс First экстендится от класса TestBase

--- exsample/src/test/java/First.java
+++ exsample/src/test/java/First.java
@@ -7,32 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class First {
-    String SITE_URL="https://mvnrepository.com/";
-    WebDriver driver;                     //Объявление переменной
-
-    @BeforeEach                           //Анотация объвляет что испоьзуемый метод будет применяться перед каждым тестолвым методом
-    public void start() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();      //Переменной присваивается созданный объект WebDriver
-        driver.manage().window().maximize();  // Окно браузера с максимальным размером
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);  // Время ожидания
-    }
-
-    @AfterEach
-    public void finish() {
-        driver.quit();
-    }
+public class First extends TestBase {
 
     @Test
     public void firstTest() {
         driver.get(SITE_URL);
-
     }
 
     @Test
     public void secondTest() {
         driver.get(SITE_URL);
-
     }
 }
